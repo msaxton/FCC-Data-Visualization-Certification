5c03cd0dac616ddfeceeb70d45f88a7aff1b3814
msaxton <msaxton@iliff.edu>
2019-08-28 17:20:51 -0600

    add legend

--- Choropleth Map Project/choropleth.js
+++ Choropleth Map Project/choropleth.js
@@ -8,8 +8,19 @@ var svg = d3.select("#visualization")
             .attr("width", w)
             .attr("height", h);
 
+// color scheme
+var color = d3.scaleThreshold()
+              .domain(d3.range(2.6, 75.1, (75.1 - 2.6) / 8))  // figure out a way so this isn't hard coded
+              .range(d3.schemeBlues[9]);
 
 // legend
+var legendWidth = 300;
+var legendHeight = 30;
+var legendScale = d3.scaleLinear()
+                    .domain([2.6, 75.1])  // figure out a way so that this doesn't have to be hard coded
+                    .range([0, legendWidth]);
+
+
 
 // tooltip
 var tooltip = d3.select("body")
